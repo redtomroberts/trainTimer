bb03def046aee1bff1362f5344716c1a650d655d
redtomroberts <55162586+redtomroberts@users.noreply.github.com>
2019-11-05 08:41:57 -0500

    Add files via upload
    Final Commit for Week 7 Homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-    var config = {
-        apiKey: "AIzaSyBPCAxjycmuYaeY8RISjpPzUOSfmZIbQrQ",
-        authDomain: "train-scheduler-68643.firebaseapp.com",
-        databaseURL: "https://train-scheduler-68643.firebaseio.com",
-        projectId: "train-scheduler-68643",
-        storageBucket: "train-scheduler-68643.appspot.com",
-        messagingSenderId: "934455543386"
+    var firebaseConfig = {
+        apiKey: 'AIzaSyD1CNDBu-iOR-NhKbOkscWL8Xa5KkcV10k',
+        authDomain: 'mbta-scheduler-8c354.firebaseapp.com',
+        databaseURL: 'https://mbta-scheduler-8c354.firebaseio.com',
+        projectId: 'mbta-scheduler-8c354',
+        storageBucket: 'mbta-scheduler-8c354.appspot.com',
+        messagingSenderId: '1057861022473',
+        appId: '1:1057861022473:web:57e7edeae5a781a5c999d8',
+        measurementId: 'G-XSBBPJ4W1F'
     };
-    firebase.initializeApp(config);
+    firebase.initializeApp(firebaseConfig);
 
     // A variable to reference the database.
     var database = firebase.database();
@@ -18,13 +20,13 @@ $(document).ready(function(){
     var firstTrain;
     var frequency = 0;
 
-    $("#add-train").on("click", function() {
+    $('#add-train').on('click', function() {
         event.preventDefault();
         // Storing and retreiving new train data
-        name = $("#train-name").val().trim();
-        destination = $("#destination").val().trim();
-        firstTrain = $("#first-train").val().trim();
-        frequency = $("#frequency").val().trim();
+        name = $('#train-name').val().trim();
+        destination = $('#destination').val().trim();
+        firstTrain = $('#first-train').val().trim();
+        frequency = $('#frequency').val().trim();
 
         // Pushing to database
         database.ref().push({
@@ -34,39 +36,38 @@ $(document).ready(function(){
             frequency: frequency,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
-        $("form")[0].reset();
+        $('form')[0].reset();
     });
 
-    database.ref().on("child_added", function(childSnapshot) {
+    database.ref().on('child_added', function(childSnapshot) {
         var nextArr;
         var minAway;
-        // Chang year so first train comes before now
-        var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+        var firstTrainNew = moment(childSnapshot.val().firstTrain, 'hh:mm');
         // Difference between the current and firstTrain
-        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+        var diffTime = moment().diff(moment(firstTrainNew), 'minutes');
         var remainder = diffTime % childSnapshot.val().frequency;
         // Minutes until next train
         var minAway = childSnapshot.val().frequency - remainder;
         // Next train time
-        var nextTrain = moment().add(minAway, "minutes");
-        nextTrain = moment(nextTrain).format("hh:mm");
+        var nextTrain = moment().add(minAway, 'minutes');
+        nextTrain = moment(nextTrain).format('hh:mm');
 
-        $("#add-row").append("<tr><td>" + childSnapshot.val().name +
-                "</td><td>" + childSnapshot.val().destination +
-                "</td><td>" + childSnapshot.val().frequency +
-                "</td><td>" + nextTrain + 
-                "</td><td>" + minAway + "</td></tr>");
+        $('#add-row').append('<tr><td>' + childSnapshot.val().name +
+                '</td><td>' + childSnapshot.val().destination +
+                '</td><td>' + childSnapshot.val().frequency +
+                '</td><td>' + nextTrain + 
+                '</td><td>' + minAway + '</td></tr>');
 
             // Handle the errors
         }, function(errorObject) {
-            console.log("Errors handled: " + errorObject.code);
+            console.log('Errors handled: ' + errorObject.code);
     });
 
-    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+    database.ref().orderByChild('dateAdded').limitToLast(1).on('child_added', function(snapshot) {
         // Change the HTML to reflect
-        $("#name-display").html(snapshot.val().name);
-        $("#email-display").html(snapshot.val().email);
-        $("#age-display").html(snapshot.val().age);
-        $("#comment-display").html(snapshot.val().comment);
+        $('#name-display').html(snapshot.val().name);
+        $('#email-display').html(snapshot.val().email);
+        $('#age-display').html(snapshot.val().age);
+        $('#comment-display').html(snapshot.val().comment);
     });
 });
